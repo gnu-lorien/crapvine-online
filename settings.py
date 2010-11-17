5b70255bdb48486e3a467750bc1381d4a488bdc9
Andrew Sayman <andrew.sayman@gmail.com>
2010-11-16 21:35:19 -0500

    Social updating stuff

--- settings.py
+++ settings.py
@@ -12,66 +12,74 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(PROJECT_ROOT, "..", "crapvine"))
 
 # tells Pinax to use the default theme
-PINAX_THEME = 'default'
+PINAX_THEME = "default"
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# tells Pinax to serve media through django.views.static.serve.
+# tells Pinax to serve media through the staticfiles app.
 SERVE_MEDIA = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+ADMINS = [
+    # ("Your Name", "your_email@domain.com"),
+]
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'dev.db'       # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "dev.db",                       # Or path to database file if using sqlite3.
+        "USER": "",                             # Not used with sqlite3.
+        "PASSWORD": "",                         # Not used with sqlite3.
+        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
-# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-# although not all variations may be possible on all operating systems.
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'US/Eastern'
+TIME_ZONE = "US/Eastern"
 
 # Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-# http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en'
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = False
+USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
 
-# URL that handles the media served from MEDIA_ROOT.
-# Example: "http://media.lawrence.com"
-MEDIA_URL = '/site_media/media/'
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = "/site_media/media/"
 
 # Absolute path to the directory that holds static files like app media.
 # Example: "/home/media/media.lawrence.com/apps/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
 
 # URL that handles the static files like app media.
 # Example: "http://media.lawrence.com"
-STATIC_URL = '/site_media/static/'
+STATIC_URL = "/site_media/static/"
 
 # Additional directories which hold static files
-STATICFILES_DIRS = (
-    ('test-crapvine-online', os.path.join(PROJECT_ROOT, 'media')),
-    ('pinax', os.path.join(PINAX_ROOT, 'media', PINAX_THEME)),
-)
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, "media"),
+    os.path.join(PINAX_ROOT, "media", PINAX_THEME),
+]
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -79,160 +87,169 @@ STATICFILES_DIRS = (
 ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'ok!9kiv=q%%qu_c)xe+wpuc0br2$r18ij!5c1vb^itgbf5f0&e'
+SECRET_KEY = "negy_23_avv95sw0lg@-u=h#$l(n2y3s2iru*li(#0l#zl+#7d"
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
+TEMPLATE_LOADERS = [
+    "django.template.loaders.filesystem.load_template_source",
+    "django.template.loaders.app_directories.load_template_source",
+]
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_openid.consumer.SessionConsumer',
-    'account.middleware.LocaleMiddleware',
-    'django.middleware.doc.XViewMiddleware',
-    'pagination.middleware.PaginationMiddleware',
-    'django_sorting.middleware.SortingMiddleware',
-    'djangodblog.middleware.DBLogMiddleware',
-    'pinax.middleware.security.HideSensistiveFieldsMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
-    'reversion.middleware.RevisionMiddleware',
-)
+MIDDLEWARE_CLASSES = [
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django_openid.consumer.SessionConsumer",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "groups.middleware.GroupAwareMiddleware",
+    "pinax.apps.account.middleware.LocaleMiddleware",
+    "django.middleware.doc.XViewMiddleware",
+    "pagination.middleware.PaginationMiddleware",
+    "django_sorting.middleware.SortingMiddleware",
+    "pinax.middleware.security.HideSensistiveFieldsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
 
-ROOT_URLCONF = 'test-crapvine-online.urls'
+ROOT_URLCONF = "crapinstall.urls"
 
-TEMPLATE_DIRS = (
+TEMPLATE_DIRS = [
     os.path.join(PROJECT_ROOT, "templates"),
     os.path.join(PINAX_ROOT, "templates", PINAX_THEME),
-)
+]
 
-TEMPLATE_CONTEXT_PROCESSORS = (
+TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+    
+    "staticfiles.context_processors.static_url",
     
     "pinax.core.context_processors.pinax_settings",
     
+    "pinax.apps.account.context_processors.account",
+    
     "notification.context_processors.notification",
     "announcements.context_processors.site_wide_announcements",
-    "account.context_processors.openid",
-    "account.context_processors.account",
     "messages.context_processors.inbox",
     "friends_app.context_processors.invitations",
-    "test-crapvine-online.context_processors.combined_inbox_count",
-)
+    
+    "crapinstall.context_processors.combined_inbox_count",
+]
 
-COMBINED_INBOX_COUNT_SOURCES = (
+COMBINED_INBOX_COUNT_SOURCES = [
     "messages.context_processors.inbox",
     "friends_app.context_processors.invitations",
     "notification.context_processors.notification",
-)
+]
 
-INSTALLED_APPS = (
-    # included
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.humanize',
-    'django.contrib.markup',
-    'pinax.templatetags',
+INSTALLED_APPS = [
+    # Django
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.humanize",
+    "django.contrib.markup",
+    
+    "pinax.templatetags",
     
     # external
-    'notification', # must be first
-    'django_openid',
-    'emailconfirmation',
-    'django_extensions',
-    'robots',
-    'friends',
-    'mailer',
-    'messages',
-    'announcements',
-    'oembed',
-    'djangodblog',
-    'pagination',
-#    'gravatar',
-    'threadedcomments',
-    'threadedcomments_extras',
-    'wiki',
-    'swaps',
-    'timezones',
-    'voting',
-    'voting_extras',
-    'tagging',
-    'bookmarks',
-    'blog',
-    'ajax_validation',
-    'photologue',
-    'avatar',
-    'flag',
-    'microblogging',
-    'locations',
-    'uni_form',
-    'django_sorting',
-    'django_markup',
-    'staticfiles',
+    "notification", # must be first
+    "staticfiles",
+    "debug_toolbar",
+    "mailer",
+    "uni_form",
+    "django_openid",
+    "ajax_validation",
+    "timezones",
+    "emailconfirmation",
+    "announcements",
+    "pagination",
+    "friends",
+    "messages",
+    "oembed",
+    "groups",
+    "threadedcomments",
+    "wakawaka",
+    "swaps",
+    "voting",
+    "tagging",
+    "bookmarks",
+    "photologue",
+    "avatar",
+    "flag",
+    "microblogging",
+    "locations",
+    "django_sorting",
+    "django_markup",
+    "tagging_ext",
     
-    # internal (for now)
-    'analytics',
-    'profiles',
-    'account',
-    'signup_codes',
-    'chronicles',
-    'photos',
-    'tag_app',
-    'topics',
-    'groups',
-
-    # Hate! (for now)
     'reversion',
     'characters',
     'authority',
+    # Pinax
+    "pinax.apps.account",
+    "pinax.apps.signup_codes",
+    "pinax.apps.analytics",
+    "pinax.apps.profiles",
+    "pinax.apps.blog",
+    "pinax.apps.tribes",
+    "pinax.apps.photos",
+    "pinax.apps.topics",
+    "pinax.apps.threadedcomments_extras",
+    "pinax.apps.voting_extras",
+    
+    # project
+    "about",
+]
 
-    'django.contrib.admin',
+FIXTURE_DIRS = [
+    os.path.join(PROJECT_ROOT, "fixtures"),
+]
 
-)
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 ABSOLUTE_URL_OVERRIDES = {
     "auth.user": lambda o: "/profiles/profile/%s/" % o.username,
 }
 
-MARKUP_FILTER_FALLBACK = 'none'
-MARKUP_CHOICES = (
-    ('restructuredtext', u'reStructuredText'),
-    ('textile', u'Textile'),
-    ('markdown', u'Markdown'),
-    ('creole', u'Creole'),
-)
-WIKI_MARKUP_CHOICES = MARKUP_CHOICES
+MARKUP_FILTER_FALLBACK = "none"
+MARKUP_CHOICES = [
+    ("restructuredtext", u"reStructuredText"),
+    ("textile", u"Textile"),
+    ("markdown", u"Markdown"),
+    ("creole", u"Creole"),
+]
 
-AUTH_PROFILE_MODULE = 'profiles.Profile'
-NOTIFICATION_LANGUAGE_MODULE = 'account.Account'
+AUTH_PROFILE_MODULE = "profiles.Profile"
+NOTIFICATION_LANGUAGE_MODULE = "account.Account"
 
 ACCOUNT_OPEN_SIGNUP = True
 ACCOUNT_REQUIRED_EMAIL = False
 ACCOUNT_EMAIL_VERIFICATION = False
+ACCOUNT_EMAIL_AUTHENTICATION = False
+ACCOUNT_UNIQUE_EMAIL = EMAIL_CONFIRMATION_UNIQUE_EMAIL = False
+
+AUTHENTICATION_BACKENDS = [
+    "pinax.apps.account.auth_backends.AuthenticationBackend",
+]
+
+LOGIN_URL = "/account/login/" # @@@ any way this can be a url name?
+LOGIN_REDIRECT_URLNAME = "what_next"
 
 EMAIL_CONFIRMATION_DAYS = 2
 EMAIL_DEBUG = DEBUG
-CONTACT_EMAIL = "feedback@example.com"
-SITE_NAME = "Crapvine Online"
-LOGIN_URL = "/account/login/"
-LOGIN_REDIRECT_URLNAME = "what_next"
-
-INTERNAL_IPS = (
-    '127.0.0.1',
-)
 
 ugettext = lambda s: s
-LANGUAGES = (
-    ('en', u'English'),
-)
+LANGUAGES = [
+    ("en", u"English"),
+]
 
 # URCHIN_ID = "ua-..."
 
@@ -245,11 +262,11 @@ class NullStream(object):
     writelines = write
 
 RESTRUCTUREDTEXT_FILTER_SETTINGS = {
-    'cloak_email_addresses': True,
-    'file_insertion_enabled': False,
-    'raw_enabled': False,
-    'warning_stream': NullStream(),
-    'strip_comments': True,
+    "cloak_email_addresses": True,
+    "file_insertion_enabled": False,
+    "raw_enabled": False,
+    "warning_stream": NullStream(),
+    "strip_comments": True,
 }
 
 # if Django is running behind a proxy, we need to do things like use
@@ -259,11 +276,13 @@ BEHIND_PROXY = False
 
 FORCE_LOWERCASE_TAGS = True
 
-WIKI_REQUIRES_LOGIN = True
-
 # Uncomment this line after signing up for a Yahoo Maps API key at the
 # following URL: https://developer.yahoo.com/wsregapp/
-# YAHOO_MAPS_API_KEY = ''
+# YAHOO_MAPS_API_KEY = ""
+
+DEBUG_TOOLBAR_CONFIG = {
+    "INTERCEPT_REDIRECTS": False,
+}
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls.defaults import *
 from django.conf import settings
+from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
@@ -8,96 +8,89 @@ admin.autodiscover()
 import authority
 authority.autodiscover()
 
-from account.openid_consumer import PinaxConsumer
-from blog.feeds import BlogFeedAll, BlogFeedUser
 from bookmarks.feeds import BookmarkFeed
+from bookmarks.models import BookmarkInstance
 from microblogging.feeds import TweetFeedAll, TweetFeedUser, TweetFeedUserWithFriends
+from microblogging.models import Tweet
+from swaps.models import Offer
+from tagging.models import TaggedItem
+from wakawaka.models import WikiPage
+
+from pinax.apps.account.openid_consumer import PinaxConsumer
+from pinax.apps.blog.feeds import BlogFeedAll, BlogFeedUser
+from pinax.apps.blog.models import Post
+from pinax.apps.photos.models import Image
+from pinax.apps.topics.models import Topic
+from pinax.apps.tribes.models import Tribe
+
+
+handler500 = "pinax.views.server_error"
 
 
 tweets_feed_dict = {"feed_dict": {
-    'all': TweetFeedAll,
-    'only': TweetFeedUser,
-    'with_friends': TweetFeedUserWithFriends,
+    "all": TweetFeedAll,
+    "only": TweetFeedUser,
+    "with_friends": TweetFeedUserWithFriends,
 }}
 
 blogs_feed_dict = {"feed_dict": {
-    'all': BlogFeedAll,
-    'only': BlogFeedUser,
+    "all": BlogFeedAll,
+    "only": BlogFeedUser,
 }}
 
-bookmarks_feed_dict = {"feed_dict": { '': BookmarkFeed }}
+bookmarks_feed_dict = {"feed_dict": {"": BookmarkFeed }}
 
 
-if settings.ACCOUNT_OPEN_SIGNUP:
-    signup_view = "account.views.signup"
-else:
-    signup_view = "signup_codes.views.signup"
-
-
-urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {
+urlpatterns = patterns("",
+    url(r"^$", direct_to_template, {
         "template": "homepage.html",
     }, name="home"),
-    
-    url(r'^admin/invite_user/$', 'signup_codes.views.admin_invite_user', name="admin_invite_user"),
-    url(r'^account/signup/$', signup_view, name="acct_signup"),
-    
-    (r'^about/', include('about.urls')),
-    (r'^account/', include('account.urls')),
-    (r'^openid/(.*)', PinaxConsumer()),
-    (r'^bbauth/', include('bbauth.urls')),
-    (r'^authsub/', include('authsub.urls')),
-    (r'^profiles/', include('profiles.urls')),
-    (r'^blog/', include('blog.urls')),
-    (r'^tags/', include('tag_app.urls')),
-    (r'^invitations/', include('friends_app.urls')),
-    (r'^notices/', include('notification.urls')),
-    (r'^messages/', include('messages.urls')),
-    (r'^announcements/', include('announcements.urls')),
-    (r'^tweets/', include('microblogging.urls')),
-    (r'^chronicles/', include('chronicles.urls')),
-    (r'^comments/', include('threadedcomments.urls')),
-    (r'^robots.txt$', include('robots.urls')),
-    (r'^i18n/', include('django.conf.urls.i18n')),
-    (r'^bookmarks/', include('bookmarks.urls')),
-    (r'^admin/(.*)', admin.site.root),
-    (r'^photos/', include('photos.urls')),
-    (r'^avatar/', include('avatar.urls')),
-    (r'^swaps/', include('swaps.urls')),
-    (r'^flag/', include('flag.urls')),
-    (r'^locations/', include('locations.urls')),
-    
-    (r'^feeds/tweets/(.*)/$', 'django.contrib.syndication.views.feed', tweets_feed_dict),
-    (r'^feeds/posts/(.*)/$', 'django.contrib.syndication.views.feed', blogs_feed_dict),
-    (r'^feeds/bookmarks/(.*)/?$', 'django.contrib.syndication.views.feed', bookmarks_feed_dict),
     (r'^characters/', include('characters.urls')),
     (r'^authority/', include('authority.urls')),
+    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^about/", include("about.urls")),
+    url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^openid/(.*)", PinaxConsumer()),
+    url(r"^profiles/", include("pinax.apps.profiles.urls")),
+    url(r"^bbauth/", include("pinax.apps.bbauth.urls")),
+    url(r"^authsub/", include("pinax.apps.authsub.urls")),
+    url(r"^blog/", include("pinax.apps.blog.urls")),
+    url(r"^invitations/", include("friends_app.urls")),
+    url(r"^notices/", include("notification.urls")),
+    url(r"^messages/", include("messages.urls")),
+    url(r"^announcements/", include("announcements.urls")),
+    url(r"^tweets/", include("microblogging.urls")),
+    url(r"^tribes/", include("pinax.apps.tribes.urls")),
+    url(r"^comments/", include("threadedcomments.urls")),
+    url(r"^i18n/", include("django.conf.urls.i18n")),
+    url(r"^bookmarks/", include("bookmarks.urls")),
+    url(r"^photos/", include("pinax.apps.photos.urls")),
+    url(r"^avatar/", include("avatar.urls")),
+    url(r"^swaps/", include("swaps.urls")),
+    url(r"^flag/", include("flag.urls")),
+    url(r"^locations/", include("locations.urls")),
+    url(r"^feeds/tweets/(.*)/$", "django.contrib.syndication.views.feed", tweets_feed_dict),
+    url(r"^feeds/posts/(.*)/$", "django.contrib.syndication.views.feed", blogs_feed_dict),
+    url(r"^feeds/bookmarks/(.*)/?$", "django.contrib.syndication.views.feed", bookmarks_feed_dict),
 )
 
 ## @@@ for now, we'll use friends_app to glue this stuff together
-
-from photos.models import Image
 
 friends_photos_kwargs = {
     "template_name": "photos/friends_photos.html",
     "friends_objects_function": lambda users: Image.objects.filter(is_public=True, member__in=users),
 }
 
-from blog.models import Post
-
 friends_blogs_kwargs = {
     "template_name": "blog/friends_posts.html",
     "friends_objects_function": lambda users: Post.objects.filter(author__in=users),
 }
 
-from microblogging.models import Tweet
-
 friends_tweets_kwargs = {
     "template_name": "microblogging/friends_tweets.html",
-    "friends_objects_function": lambda users: Tweet.objects.filter(sender_id__in=[user.id for user in users], sender_type__name='user'),
+    "friends_objects_function": lambda users: Tweet.objects.filter(sender_id__in=[user.id for user in users], sender_type__name="user"),
 }
-
-from bookmarks.models import Bookmark
 
 friends_bookmarks_kwargs = {
     "template_name": "bookmarks/friends_bookmarks.html",
@@ -107,14 +100,52 @@ friends_bookmarks_kwargs = {
     },
 }
 
-urlpatterns += patterns('',
-    url('^photos/friends_photos/$', 'friends_app.views.friends_objects', kwargs=friends_photos_kwargs, name="friends_photos"),
-    url('^blog/friends_blogs/$', 'friends_app.views.friends_objects', kwargs=friends_blogs_kwargs, name="friends_blogs"),
-    url('^tweets/friends_tweets/$', 'friends_app.views.friends_objects', kwargs=friends_tweets_kwargs, name="friends_tweets"),
-    url('^bookmarks/friends_bookmarks/$', 'friends_app.views.friends_objects', kwargs=friends_bookmarks_kwargs, name="friends_bookmarks"),
+urlpatterns += patterns("",
+    url(r"^photos/friends_photos/$", "friends_app.views.friends_objects", kwargs=friends_photos_kwargs, name="friends_photos"),
+    url(r"^blog/friends_blogs/$", "friends_app.views.friends_objects", kwargs=friends_blogs_kwargs, name="friends_blogs"),
+    url(r"^tweets/friends_tweets/$", "friends_app.views.friends_objects", kwargs=friends_tweets_kwargs, name="friends_tweets"),
+    url(r"^bookmarks/friends_bookmarks/$", "friends_app.views.friends_objects", kwargs=friends_bookmarks_kwargs, name="friends_bookmarks"),
+)
+
+tagged_models = (
+    dict(title="Blog Posts",
+        query=lambda tag : TaggedItem.objects.get_by_model(Post, tag).filter(status=2),
+        content_template="pinax_tagging_ext/blogs.html",
+    ),
+    dict(title="Bookmarks",
+        query=lambda tag : TaggedItem.objects.get_by_model(BookmarkInstance, tag),
+        content_template="pinax_tagging_ext/bookmarks.html",
+    ),
+    dict(title="Photos",
+        query=lambda tag: TaggedItem.objects.get_by_model(Image, tag).filter(safetylevel=1),
+        content_template="pinax_tagging_ext/photos.html",
+    ),
+    dict(title="Swap Offers",
+        query=lambda tag : TaggedItem.objects.get_by_model(Offer, tag),
+    ),
+    dict(title="Topics",
+        query=lambda tag: TaggedItem.objects.get_by_model(Topic, tag),
+    ),
+    dict(title="Tribes",
+        query=lambda tag: TaggedItem.objects.get_by_model(Tribe, tag),
+    ),
+    dict(title="Wiki Articles",
+        query=lambda tag: TaggedItem.objects.get_by_model(WikiPage, tag),
+    ),
+)
+tagging_ext_kwargs = {
+    "tagged_models": tagged_models,
+}
+
+urlpatterns += patterns("",
+    url(r"^tags/(?P<tag>.+)/(?P<model>.+)$", "tagging_ext.views.tag_by_model",
+        kwargs=tagging_ext_kwargs, name="tagging_ext_tag_by_model"),
+    url(r"^tags/(?P<tag>.+)/$", "tagging_ext.views.tag",
+        kwargs=tagging_ext_kwargs, name="tagging_ext_tag"),
+    url(r"^tags/$", "tagging_ext.views.index", name="tagging_ext_index"),
 )
 
 if settings.SERVE_MEDIA:
-    urlpatterns += patterns('',
-        (r'^site_media/', include('staticfiles.urls')),
+    urlpatterns += patterns("",
+        url(r"", include("staticfiles.urls")),
     )
