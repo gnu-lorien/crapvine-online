5da08c6cdaefbb401f77d784f69fc3e99b39c496
Andrew Sayman <andrew.sayman@gmail.com>
2011-04-25 22:24:45 -0400

    Admin page improvements

--- apps/characters/admin.py
+++ apps/characters/admin.py
@@ -3,19 +3,49 @@ from django.contrib import admin
 from reversion.admin import VersionAdmin
 
 class SheetAdmin(VersionAdmin):
-    pass
+    list_display = ['name', 'player', 'last_saved', 'npc', 'status']
+    list_filter = ['status', 'npc', 'player']
+    search_fields = ['name', 'player__username']
+
 class ExpendableAdmin(VersionAdmin):
     pass
+
 class TraitAdmin(VersionAdmin):
-    pass
+    list_display = ['name', 'value', 'note', 'traitlistname', 'sheet', 'sheet_owner']
+    list_filter = ['traitlistname']
+    search_fields = ['name', 'value', 'note', 'traitlistname__name', 'sheet__name', 'sheet__player__username']
+
+    def sheet_owner(self, trait):
+        return trait.sheet.player
+
 class TraitListPropertyAdmin(VersionAdmin):
-    pass
+    list_display = ['name', 'sheet', 'sorted', 'atomic', 'negative', 'display_preference']
+    list_filter = ['sorted', 'atomic', 'negative', 'display_preference']
+    search_fields = ['name', 'sheet__name']
+
 class TraitListNameAdmin(VersionAdmin):
     pass
 class VampireSheetAdmin(VersionAdmin):
-    pass
+    list_display = ['name', 'player', 'last_saved', 'npc', 'status', 'clan', 'sect', 'title', 'path']
+    list_filter = ['status', 'npc', 'clan', 'sect', 'title', 'path', 'player']
+    search_fields = ['name', 'player__username', 'clan', 'sect', 'title', 'path']
+
 class ExperienceEntryAdmin(VersionAdmin):
     pass
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'sorted', 'negative', 'required', 'autonote', 'display_preference']
+    list_filter = ['category', 'sorted', 'negative', 'required', 'autonote', 'display_preference']
+    search_fields = ['name', 'category']
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'disp_parent', 'cost', 'note', 'order', 'item_type', 'disp_menu_to_import']
+
+    def disp_parent(self, mi):
+        return mi.parent.name
+
+    def disp_menu_to_import(self, mi):
+        return mi.menu_to_import.name
 
 admin.site.register(Sheet, SheetAdmin)
 admin.site.register(Expendable, ExpendableAdmin)
@@ -25,5 +55,5 @@ admin.site.register(TraitListName, TraitListNameAdmin)
 admin.site.register(VampireSheet, VampireSheetAdmin)
 admin.site.register(ExperienceEntry, ExperienceEntryAdmin)
 
-admin.site.register(Menu)
-admin.site.register(MenuItem)
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
