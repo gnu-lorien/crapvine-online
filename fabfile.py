e5f53cde523c97577d7b35f3d0f9151efbd0867d
Andrew Sayman <andrew.sayman@gmail.com>
2011-05-17 15:26:14 -0400

    Additional fab deployment options

--- fabfile.py
+++ fabfile.py
@@ -7,24 +7,40 @@ def prepare_deploy():
 def post_deploy():
     local("git stash pop")
 
-def deploy(scp_port, scp_server, dest_dir):
+def deploy_helper(deploy_dirs, scp_port, scp_server, dest_dir):
     prepare_deploy()
-    deploy_dirs = [
-        'apps',
-        'fixtures',
-        'templates',
-        'tests'
-    ]
     for deploy_dir in deploy_dirs:
         local("scp -rp -P {} {dd}/* {}:{dest_dir}/{dd}".format(scp_port, scp_server, dd=deploy_dir, dest_dir=dest_dir))
 
     run('./uwsgi.sh restart')
     post_deploy()
 
-def deploy_media(scp_port, scp_server):
+def deploy(scp_port, scp_server, dest_dir):
+    deploy_dirs = [
+        'apps',
+        'fixtures',
+        'templates',
+        'tests'
+    ]
+    deploy_helper(deploy_dirs, scp_port, scp_server, dest_dir)
+
+def deploy_apps(scp_port, scp_server, dest_dir):
+    deploy_dirs = [
+        'apps',
+    ]
+    deploy_helper(deploy_dirs, scp_port, scp_server, dest_dir)
+
+def deploy_templates(scp_port, scp_server, dest_dir):
+    deploy_dirs = [
+        'templates',
+    ]
+    deploy_helper(deploy_dirs, scp_port, scp_server, dest_dir)
+
+#TODO: Needs to copy correctly to the location
+def deploy_media(scp_port, scp_server, dest_dir):
     deploy_dirs = [
         'site_media/static',
         'media',
     ]
     for deploy_dir in deploy_dirs:
-        local("scp -rp -P {} {dd}/* {}:~/env/testit/{dd}".format(scp_port, scp_server, dd=deploy_dir))
+        local("scp -rp -P {} {dd}/* {}:{dest_dir}/{dd}".format(scp_port, scp_server, dd=deploy_dir, dest_dir=dest_dir))
